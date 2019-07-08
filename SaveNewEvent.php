050d067043cb902e7e69890966a07b86c023b4a4
Bethany Chen <bethany.zc@gmail.com>
2019-07-08 13:23:28 -0400

    wah

---
+++ SaveNewEvent.php
@@ -0,0 +1,105 @@
+<html><head>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                                                                         document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1417086791641274');
+fbq('track', "PageView");
+fbq('track', 'CompleteRegistration');
+
+(function() {
+    var s = {};
+    (function() {
+        var c = this || self;
+        var f = /#|$/;
+        function l(d) {
+            var g = d.search(f), a;
+            a: {
+                for (a = 0; 0 <= (a = d.indexOf("fmt", a)) && a < g; ) {
+                    var b = d.charCodeAt(a - 1);
+                    if (38 == b || 63 == b)
+                        if (b = d.charCodeAt(a + 3),
+                        !b || 61 == b || 38 == b || 35 == b)
+                            break a;
+                    a += 4
+                }
+                a = -1
+            }
+            if (0 > a)
+                return null;
+            b = d.indexOf("&", a);
+            if (0 > b || b > g)
+                b = g;
+            a += 4;
+            return decodeURIComponent(d.substr(a, b - a).replace(/\+/g, " "))
+        }
+        ;function n(d, g, a) {
+            function b() {
+                --q;
+                if (0 >= q) {
+                    var e;
+                    (e = d.GooglebQhCsO) || (e = {});
+                    var r = e[g];
+                    r && (delete e[g],
+                    (e = r[0]) && e.call && e())
+                }
+            }
+            for (var q = a.length + 1, m = 0; m < a.length; m++) {
+                var h = l(a[m])
+                  , k = null;
+                1 != h && 2 != h || !(h = d.document.getElementById("goog_conv_iframe")) || h.src || (k = h);
+                k || (k = new Image);
+                k.onload = b;
+                k.src = a[m]
+            }
+            b()
+        }
+        var p = ["ss_"]
+          , t = s || c;
+        p[0]in t || "undefined" == typeof t.execScript || t.execScript("var " + p[0]);
+        for (var u; p.length && (u = p.shift()); ) {
+            var v;
+            if (v = !p.length)
+                v = void 0 !== n;
+            v ? t[u] = n : t[u] && t[u] !== Object.prototype[u] ? t = t[u] : t = t[u] = {}
+        }
+        ;
+    }
+    ).call(this);
+    ;s.ss_(window, 'aEZIRkNPakNwV2tRNnFUeHhnTToxNTYyNjAxNzQ2MDg2', ['https://googleads.g.doubleclick.net/pagead/viewthroughconversion/953963114/?random\x3d473849574\x26cv\x3d9\x26fst\x3d*\x26num\x3d1\x26label\x3dhFHFCOjCpWkQ6qTxxgM\x26bg\x3dffffff\x26hl\x3den\x26guid\x3dON\x26resp\x3dGooglemKTybQhCsO\x26u_h\x3d1080\x26u_w\x3d1920\x26u_ah\x3d1040\x26u_aw\x3d1920\x26u_cd\x3d24\x26u_his\x3d12\x26u_tz\x3d-240\x26u_java\x3dfalse\x26u_nplug\x3d3\x26u_nmime\x3d4\x26sendb\x3d1\x26frm\x3d0\x26url\x3dhttps://www.when2meet.com/SaveNewEvent.php\x26ref\x3dhttps://www.when2meet.com/\x26fmt\x3d3\x26ctc_id\x3dCAIVAgAAAB0CAAAA\x26ct_cookie_present\x3dfalse\x26ocp_id\x3dG2kjXZHWDNWAMfmEtcgO\x26crd\x3d\x26gtd\x3d']);
+}
+)();
+
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1417086791641274&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+</head>
+<body onload="window.location='/?7927307-XDIFy'">
+<!-- Google Code for w2m survey creation Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 953963114;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "hFHFCOjCpWkQ6qTxxgM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/953963114/?label=hFHFCOjCpWkQ6qTxxgM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+</body></html>
+
+
+
